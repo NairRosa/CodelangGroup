9d7ce4e59bb3769b4d77c742eb253e3a1cd8c4c5
NairNeta <contatonairrosa@gmail.com>
2023-04-23 22:22:21 -0300

    Add files via upload

---
+++ Exercício 2/Script.js
@@ -0,0 +1,9 @@
+var idade = prompt("Qual é a sua idade?");
+
+if (idade >= 18 && idade <= 60 ){
+    alert("Sua idade é: " + idade + "\n" + "Você pode participar do processo seletivo.");
+    console.log(idade + " Você pode participar do processo seletivo.");
+} else {
+    alert("Sua idade é: " + idade + "\n" + "Infelizmente, você não pode participar do processo seletivo.");
+    console.log(idade + " Infelizmente22, você não pode participar do processo seletivo.");
+}
