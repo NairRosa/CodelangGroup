05e1eba455b9b7f35a92b1db317d0259cd10905e
NairNeta <contatonairrosa@gmail.com>
2023-05-09 22:59:19 -0300

    Add files via upload

--- Exercício 6/Script.js
+++ Exercício 6/Script.js
@@ -4,11 +4,15 @@ function imparOuPar() {
     do {
         numero = prompt("Insira um número pra saber se ele é ímpar ou par:")
         let resultado = numero % 2 
-        if (resultado === 0) {
-            alert("O número " + numero + "," + " é par.");
+        
+        if (numero > 0 && resultado === 0) {
+            alert("O número " + numero + "," + " é positivo e par.");
         }
-        if (resultado != 0) {
-            alert("O número " + numero + "," + " é ímpar.");
+        else if (numero > 0 && resultado != 0) {
+            alert("O número " + numero + "," + " é positivo e ímpar.");
+        }
+        else {
+            alert("O número " + numero + "," + " é negativo.");
         }
     } while (resultado >= 0)
 }
